aff76fe513ad2dc8baf5f7d296118e7f03475f1d
Joost Damad <joost@damad.be>
2015-12-31 14:45:38 +0100

    parsing work

--- src/lib.rs
+++ src/lib.rs
@@ -31,26 +31,56 @@ impl Module {
     fn new(name: &String) -> Module {
         Module { name: name.clone(), elements: vec![] }
     }
+    fn append(&mut self, e: Element) {
+        self.elements.push(e)
+    }
 }
 
 impl fmt::Display for Module {
     fn fmt(&self, f: &mut fmt::Formatter) -> Result<(), fmt::Error> {
-        write!(f, "Module({})", self.name)
+        try!(write!(f, "(module {}\n", self.name));
+        for e in &self.elements {
+            try!(write!(f, "{}\n", e))
+        };
+        write!(f, ")")
     }
 }
 
+impl fmt::Display for Element {
+    fn fmt(&self, f: &mut fmt::Formatter) -> Result<(), fmt::Error> {
+        match *self {
+            Element::Layer => write!(f, "(layer)"),
+            Element::Descr => write!(f, "(desc)"),
+            Element::FpText => write!(f, "(fp_text)"),
+            Element::Pad => write!(f, "(pad)"),
+            Element::FpPoly => write!(f, "(fp_poly)"),
+            Element::FpLine => write!(f, "(fp_line)"),
+            Element::FpCircle => write!(f, "(fp_circle)"),
+        }
+    }
+}
+
+fn parse_element(s: &Sexp) -> ERes<Element> {
+    Ok(Element::Layer)
+}
+
 fn parse_module_list(v: Vec<Sexp>) -> ERes<Module> {
-    match v[0] {
+    let mut module = (match v[0] {
         Sexp::Atom(Atom::S(ref s)) if s == "module" => {
             match v[1] {
                 Sexp::Atom(Atom::S(ref s)) => {
                     Ok(Module::new(s))
                 }
-                _ => err("expecting module name")
+                _ => return err("expecting module name")
             }
         }
         _ => err("expecting module")
+    }).unwrap();
+    for e in &v[2..] {
+        let el = try!(parse_element(e));
+        module.append(el)
     }
+    Ok(module)
 }
 
 fn parse_module(s: Sexp) -> ERes<Module> {
