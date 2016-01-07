8259edeffd68d9cbebe2e9ec2c643cd49c8a020a
Joost Damad <joost@damad.be>
2016-01-07 18:43:58 +0100

    component & descr WIP

--- src/schematic.rs
+++ src/schematic.rs
@@ -7,18 +7,35 @@ use ERes;
 use err;
 use read_file;
 
+#[derive(Debug)]
 pub struct Schematic {
-    libraries:Vec<String>
+    libraries:Vec<String>,
+    description:Description,
+    components:Vec<Component>,
 }
 
 impl Schematic {
     fn new() -> Schematic {
-        Schematic { libraries:vec![] }
+        Schematic {
+            libraries:vec![],
+            description:Description::new(),
+            components:vec![],
+        }
     }
 
     fn add_library(&mut self, s:&str) {
         self.libraries.push(String::from(s))
     }
+
+    fn set_description(&mut self, d:&Description) {
+        self.description.clone_from(d)
+    }
+
+    fn append_component(&mut self, c:Component) {
+        self.components.push(c)
+    }
+    
+    
 }
 
 impl fmt::Display for Schematic {
@@ -26,6 +43,10 @@ impl fmt::Display for Schematic {
         try!(writeln!(f, "EESchema Schematic File Version 2"));
         for v in &self.libraries[..] {
             try!(writeln!(f, "LIBS:{}", v))
+        }
+        try!(write!(f, "{}", self.description));
+        for v in &self.components[..] {
+            try!(write!(f, "{}", v))
         }
         Ok(())
     }
@@ -43,6 +64,7 @@ macro_rules! assume_line {
     )
 }
 
+#[derive(Debug)]
 struct ParseState {
     i:usize,
     v:Vec<String>,
@@ -53,12 +75,60 @@ impl ParseState {
         return &self.v[self.i]
     }
 
-    fn next(&mut self) {
+    fn next(&mut self) -> () {
         self.i += 1
     }
 
     fn eof(&self) -> bool {
         self.i >= self.v.len()
+    }
+    fn parse_description(&mut self) -> ERes<Description> {
+        Ok(Description::new())
+    }
+    fn parse_component(&mut self) -> ERes<Component> {
+        Ok(Component::new())
+    }
+}
+
+#[derive(Debug,Clone)]
+pub struct Description {
+    size:String
+}
+
+impl Description {
+    fn new() -> Description {
+        Description { size:String::from("A4") }
+    }
+}
+
+impl fmt::Display for Description {
+    fn fmt(&self, f: &mut fmt::Formatter) -> Result<(), fmt::Error> {
+        try!(writeln!(f, "$Descr {}", self.size));
+        writeln!(f, "$EndDescr")
+    }
+}
+
+
+#[derive(Debug)]
+struct Component {
+    name:String,
+    reference:String,
+}
+
+impl Component {
+    fn new() -> Component {
+        Component {
+            name:String::from("EFM32HG310"),
+            reference:String::from("U1"),
+        }
+    }
+}
+
+impl fmt::Display for Component {
+    fn fmt(&self, f: &mut fmt::Formatter) -> Result<(), fmt::Error> {
+        try!(writeln!(f, "$Comp"));
+        try!(writeln!(f, "L {} {}", self.name, self.reference));
+        writeln!(f, "$EndComp")
     }
 }
 
@@ -82,6 +152,17 @@ fn parse(s: &str) -> ERes<Schematic> {
     assume_line!(p, "EELAYER 25 0");
     assume_line!(p, "EELAYER END");
     while !p.eof() {
+        match &p.here()[..] {
+            "$Descr" => {
+                let d = try!(p.parse_description());
+                sch.set_description(&d)
+            },
+            "$Comp" => {
+                let c = try!(p.parse_component());
+                sch.append_component(c)
+            },
+            _ => println!("TODO: parse other")
+        }
         p.next()
     }
     Ok(sch)
