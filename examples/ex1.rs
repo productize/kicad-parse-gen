bae404b7a8d80393c416ad43a9af71d682310ae6
Joost Damad <joost@damad.be>
2016-01-07 14:23:14 +0100

    change file hierarchy use modules :)

--- examples/ex1.rs
+++ examples/ex1.rs
@@ -1,8 +1,8 @@
-// (c) 2015 Joost Yervante Damad <joost@damad.be>
+// (c) 2015-2016 Joost Yervante Damad <joost@productize.be>
 
 extern crate rustykicad;
 
 fn main() {
-    let s = rustykicad::parse_file("../examples/SILABS_EFM32_QFN24.kicad_mod");
+    let s = rustykicad::footprint::parse_file("../examples/SILABS_EFM32_QFN24.kicad_mod");
     println!("{}", s);
 }
