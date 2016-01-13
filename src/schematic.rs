d7cc9faa26eb49528452f406251f9fc4feb14079
Joost Damad <joost@damad.be>
2016-01-13 15:46:52 +0100

    component field parsing WIP

--- src/schematic.rs
+++ src/schematic.rs
@@ -186,29 +186,149 @@ impl fmt::Display for Description {
     }
 }
 
-
 #[derive(Debug)]
 struct Component {
     name:String,
     reference:String,
+    u:String, // TODO
+    x:f64,
+    y:f64,
+    fields:Vec<ComponentField>,
 }
 
 impl Component {
     fn new() -> Component {
         Component {
-            name:String::from("EFM32HG310"),
+            name:String::from("DUMMY"),
             reference:String::from("U1"),
+            u:String::from(""),
+            x:0.0,
+            y:0.0,
+            fields:vec![],
         }
     }
+
+    fn set_name(&mut self, s:&String) {
+        self.name.clone_from(s)
+    }
+    fn set_reference(&mut self, s:&String) {
+        self.reference.clone_from(s)
+    }
+    fn set_u(&mut self, s:&String) {
+        self.u.clone_from(s)
+    }
+    fn set_x(&mut self, x:f64) {
+        self.x = x
+    }
+    fn set_y(&mut self, y:f64) {
+        self.y = y
+    }
+    fn add_field(&mut self, f:ComponentField) {
+        self.fields.push(f)
+    }
+    
 }
 
 impl fmt::Display for Component {
     fn fmt(&self, f: &mut fmt::Formatter) -> Result<(), fmt::Error> {
         try!(writeln!(f, "$Comp"));
         try!(writeln!(f, "L {} {}", self.name, self.reference));
+        try!(writeln!(f, "{}", self.u));
+        try!(writeln!(f, "P {} {}", self.x, self.y));
         writeln!(f, "$EndComp")
     }
 }
+
+#[derive(Debug)]
+enum Orientation {
+    Horizontal,
+    Vertical
+}
+
+impl Orientation {
+    fn new(c:char) -> ERes<Orientation> {
+        match c {
+            'H' => Ok(Orientation::Horizontal),
+            'V' => Ok(Orientation::Vertical),
+            _ => Err(format!("unknown orientation {}", c))
+        }
+        
+    }
+}
+
+
+#[derive(Debug)]
+enum Justify {
+    Left,
+    Right,
+    Center,
+    Bottom,
+    Top,
+}
+
+impl Justify {
+    fn new(c: char) -> ERes<Justify> {
+        match c {
+            'C' => Ok(Justify::Center),
+            'R' => Ok(Justify::Right),
+            'L' => Ok(Justify::Left),
+            'B' => Ok(Justify::Bottom),
+            'T' => Ok(Justify::Top),
+            _ => Err(format!("unexpected justify: {}", c))
+        }
+    }
+}
+
+
+#[derive(Debug)]
+struct ComponentField {
+    i:i64,
+    value:String,
+    orientation:Orientation,
+    x:f64,
+    y:f64,
+    size:i64,
+    visible:bool,
+    hjustify:Justify,
+    vjustify:Justify,
+    italic:bool,
+    bold:bool,
+    name:Option<String>,
+}
+
+impl ComponentField {
+    fn new(v:&Vec<String>) -> ERes<ComponentField> {
+        if v.len() != 10 && v.len() != 11 {
+            return Err(format!("expecting 10 or 11 parts got {}", v.len()))
+        }
+        let name = if v.len() == 11 {
+            Some(try!(unquote_string(&v[10])))
+        } else {
+            None
+        };
+        let c = ComponentField {
+            i:try!(i64_from_string(&v[1])),
+            value:try!(unquote_string(&v[2])),
+            orientation:try!(Orientation::new(char_at(&v[3],0))),
+            x:try!(f64_from_string(&v[4])),
+            y:try!(f64_from_string(&v[5])),
+            size:try!(i64_from_string(&v[6])),
+            visible:try!(bool_from_string(&v[7],"0000","0001")),
+            hjustify:try!(Justify::new(char_at(&v[8], 0))),
+            vjustify:try!(Justify::new(char_at(&v[9], 0))),
+            italic:try!(bool_from(char_at(&v[9], 1), 'I', 'N')),
+            bold:try!(bool_from(char_at(&v[9], 1), 'B', 'N')),
+            name:name,
+        };
+        Ok(c)
+    }
+}
+
+fn char_at(s:&String, p:usize) -> char {
+    let v:Vec<char> = s.chars().collect();
+    v[..][p]
+}
+
 
 fn assume_string(e:&'static str, s:&String) -> ERes<()> {
     if String::from(e) != *s {
@@ -225,12 +345,72 @@ fn parse_split_n(n:usize, s:&String) -> ERes<Vec<String>> {
     return Ok(sp.iter().map(|x| String::from(*x)).collect())
 }
 
+fn parse_split_quote_aware(s:&String) -> Vec<String> {
+    let mut v:Vec<String> = vec![];
+    let mut in_q:bool = false;
+    let mut s2:String = String::from("");
+    for c in s.chars() {
+        if !in_q && c == '\"' {
+            in_q = true;
+            s2.push(c);
+            continue
+        }
+        if in_q && c == '\"' {
+            in_q = false;
+            s2.push(c);
+            continue
+        }
+        if c == ' ' {
+            if s2.len() > 0 {
+                v.push(s2.clone());
+                s2.clear();
+            }
+            continue;
+        }
+        s2.push(c);
+    }
+    if s2.len() > 0 {
+        v.push(s2.clone())
+    }
+    return v
+    
+}
+
 fn i64_from_string(s:&String) -> ERes<i64> {
     match i64::from_str(&s[..]) {
         Ok(i) => Ok(i),
         _ => Err(format!("int parse error in {}", s))
     }
 }
+
+fn f64_from_string(s:&String) -> ERes<f64> {
+    match f64::from_str(&s[..]) {
+        Ok(i) => Ok(i),
+        _ => Err(format!("float parse error in {}", s))
+    }
+}
+
+fn bool_from_string(s:&String, t:&'static str, f:&'static str) -> ERes<bool> {
+    if &s[..] == t {
+        return Ok(true)
+    }
+    if &s[..] == f {
+        return Ok(false)
+    }
+    Err(format!("unknown boolean {}, expected {} or {}", s, t, f))
+}
+
+fn bool_from<T: PartialEq + fmt::Display>(i:T, t:T, f:T) -> ERes<bool> {
+    if i == t {
+        return Ok(true)
+    }
+    if i == f {
+        return Ok(false)
+    }
+    Err(format!("unknown boolean {}, expected {} or {}", i, t, f))
+}
+
+
 
 // this is not perfect
 fn unquote_string(s:&String) -> ERes<String> {
@@ -243,6 +423,7 @@ fn unquote_string(s:&String) -> ERes<String> {
     }
     Err(format!("expecting quoted string: {}", s))
 }
+
 
 fn word_and_qstring<F>(d:&mut Description, name:&'static str, s:&String, setter:F) -> ERes<()>
     where F:Fn(&mut Description, &String) -> ()
@@ -274,18 +455,61 @@ fn parse_description(p:&mut ParseState) -> ERes<Description> {
     p.next();
     try!(word_and_qstring(&mut d, "Comment1", &p.here(), |d, x| d.set_comment1(x)));
     p.next();
-    try!(word_and_qstring(&mut d, "Comment2", &p.here(), |d, x| d.set_comment1(x)));
+    try!(word_and_qstring(&mut d, "Comment2", &p.here(), |d, x| d.set_comment2(x)));
     p.next();
-    try!(word_and_qstring(&mut d, "Comment3", &p.here(), |d, x| d.set_comment1(x)));
+    try!(word_and_qstring(&mut d, "Comment3", &p.here(), |d, x| d.set_comment3(x)));
     p.next();
-    try!(word_and_qstring(&mut d, "Comment4", &p.here(), |d, x| d.set_comment1(x)));
+    try!(word_and_qstring(&mut d, "Comment4", &p.here(), |d, x| d.set_comment4(x)));
     p.next();
     try!(assume_string("$EndDescr", &p.here()));
     Ok(d)
 }
 
+fn parse_component_l(p:&mut ParseState, d:&mut Component) -> ERes<()> {
+    let v = try!(parse_split_n(3, &p.here()));
+    d.set_name(&v[1]);
+    d.set_reference(&v[2]);
+    Ok(())
+}
+
+fn parse_component_u(p:&mut ParseState, d:&mut Component) -> ERes<()> {
+    d.set_u(&p.here());
+    Ok(())
+}
+
+fn parse_component_p(p:&mut ParseState, d:&mut Component) -> ERes<()> {
+    let v = try!(parse_split_n(3, &p.here()));
+    d.set_x(try!(f64_from_string(&v[1])));
+    d.set_y(try!(f64_from_string(&v[2])));
+    Ok(())
+}
+
+fn parse_component_f(p:&mut ParseState, d:&mut Component) -> ERes<()> {
+    //println!("{}", p.here());
+    let v = parse_split_quote_aware(&p.here());
+    let f = try!(ComponentField::new(&v));
+    d.add_field(f);
+    Ok(())
+}
+
+
 fn parse_component(p:&mut ParseState) -> ERes<Component> {
     let mut d = Component::new();
+    p.next();
+    loop {
+        let s = p.here();
+        if s == "$EndComp" {
+            break;
+        }
+        match s.split_whitespace().next() {
+            Some("L") => try!(parse_component_l(p, &mut d)),
+            Some("U") => try!(parse_component_u(p, &mut d)),
+            Some("P") => try!(parse_component_p(p, &mut d)),
+            Some("F") => try!(parse_component_f(p, &mut d)),
+            _ => println!("skipping unknown component line {}", s)
+        }
+        p.next()
+    }
     Ok(d)
 }
 
@@ -317,7 +541,7 @@ fn parse(s: &str) -> ERes<Schematic> {
                     let d = try!(parse_component(p));
                     sch.append_component(d)
                 },
-                Some(x) => {
+                Some(_) => {
                     //println!("TODO other parts: {}", x);
                 },
                 None => unreachable!()
