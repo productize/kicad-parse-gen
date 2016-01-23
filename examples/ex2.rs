0ed21f2302db9fe12d68c938b088f41dcc2d0a31
Joost Damad <joost@damad.be>
2016-01-23 16:00:21 +0100

    fix build

--- examples/ex2.rs
+++ examples/ex2.rs
@@ -1,8 +1,10 @@
 // (c) 2016 Joost Yervante Damad <joost@productize.be>
 
 extern crate rustykicad;
+use std::path::PathBuf;
 
 fn main() {
-    let s = rustykicad::schematic::parse_file("../examples/hgminiUSBC.sch").unwrap();
+    let f = PathBuf::from("../examples/hgminiUSBC.sch");
+    let s = rustykicad::schematic::parse_file(f).unwrap();
     println!("{}", s);
 }
