26fc45cf335ea7aeed477a34ed1a222b7eae34d0
Joost Yervante Damad <joost@productize.be>
2016-06-07 15:36:45 +0200

    make read_file public

--- src/lib.rs
+++ src/lib.rs
@@ -15,7 +15,7 @@ pub trait FromSexp {
     fn from_sexp(&Sexp) -> Self;
 }
 
-use util::read_file;
+pub use util::read_file;
 
 pub fn parse_split_quote_aware(s:&str) -> Vec<String> {
     let mut v:Vec<String> = vec![];
