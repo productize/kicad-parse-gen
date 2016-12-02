e12d81436f7aecdad4ae7abb29ae0303f1f992f1
Joost Yervante Damad <joost@damad.be>
2016-12-02 09:43:50 +0100

    fix build on stable compiler

--- src/build.rs
+++ src/build.rs
@@ -5,20 +5,17 @@ mod inner {
 
     use std::env;
     use std::path::Path;
-    use std::fs;
 
     pub fn main() {
         let out_dir = env::var_os("OUT_DIR").unwrap();
         
         let src = Path::new("src/footprint/data2.rs.in");
-        let dst_dir = Path::new(&out_dir).join("footprint");
-        fs::create_dir(&dst_dir).unwrap();
+        let dst_dir = Path::new(&out_dir);
         let dst = dst_dir.join("data2.rs");
         serde_codegen::expand(&src, &dst).unwrap();
-
+        
         let src = Path::new("src/test_data.rs.in");
         let dst_dir = Path::new(&out_dir);
-        fs::create_dir(&dst_dir).unwrap();
         let dst = dst_dir.join("test_data.rs");
         serde_codegen::expand(&src, &dst).unwrap();
     }
