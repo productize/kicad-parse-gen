4ea249ad7da211a51fea36acd244d9fddcb27e3c
Joost Yervante Damad <joost@damad.be>
2017-01-23 11:27:07 +0100

    update layout ser code to use more compacted list handling

--- src/layout/ser.rs
+++ src/layout/ser.rs
@@ -13,64 +13,59 @@ use layout::data::*;
 
 impl IntoSexp for Layout {
     fn into_sexp(&self) -> Sexp {
-        let mut v = vec![];
-        v.push("kicad_pcb".into());
+        let mut v = Sexp::start("kicad_pcb");
 
-        v.push(("version", &self.version).into());
+        v.push(("version", &self.version));
 
-        let mut v2 = vec![];
-        v2.push("host".into());
-        v2.push(self.host.tool.clone().into());
-        v2.push(self.host.build.clone().into());
-        v.push(v2.into());
+        let mut v2 = Sexp::start("host");
+        v2.push(&self.host.tool);
+        v2.push(&self.host.build);
+        v.push(v2);
 
         v.push(self.general.into_sexp());
 
-        v.push(("page", &self.page).into());
+        v.push(("page", &self.page));
 
 
-        let mut v2 = vec![];
-        v2.push("layers".into());
+        let mut v2 = Sexp::start("layers");
         for layer in &self.layers {
             v2.push(layer.into_sexp());
         }
-        v.push(v2.into());
+        v.push(v2);
 
         v.push(self.setup.into_sexp());
 
         for element in &self.elements {
             v.push(element.into_sexp());
         }
-        v.into()
+        v
     }
 }
 
 impl IntoSexp for General {
     fn into_sexp(&self) -> Sexp {
-        let mut v = vec![];
-        v.push("general".into());
-        v.push(("links", &self.links).into());
-        v.push(("no_connects", &self.no_connects).into());
+        let mut v = Sexp::start("general");
+        v.push(("links", &self.links));
+        v.push(("no_connects", &self.no_connects));
         v.push(self.area.into_sexp());
-        v.push(("thickness", &self.thickness).into());
-        v.push(("drawings", &self.drawings).into());
-        v.push(("tracks", &self.tracks).into());
-        v.push(("zones", &self.zones).into());
-        v.push(("modules", &self.modules).into());
-        v.push(("nets", &self.nets).into());
-        v.into()
+        v.push(("thickness", &self.thickness));
+        v.push(("drawings", &self.drawings));
+        v.push(("tracks", &self.tracks));
+        v.push(("zones", &self.zones));
+        v.push(("modules", &self.modules));
+        v.push(("nets", &self.nets));
+        v
     }
 }
 
 impl IntoSexp for Area {
     fn into_sexp(&self) -> Sexp {
-        let mut v = vec![];
-        v.push("area".into());
-        v.push(self.x1.into());
-        v.push(self.y1.into());
-        v.push(self.x2.into());
-        v.push(self.y2.into());
-        v.into()
+        let mut v = Sexp::start("area");
+        v.push(self.x1);
+        v.push(self.y1);
+        v.push(self.x2);
+        v.push(self.y2);
+        v
     }
 }
 
@@ -92,49 +87,46 @@ impl IntoSexp for Element {
 
 impl IntoSexp for Zone {
     fn into_sexp(&self) -> Sexp {
-        let mut v = vec![];
-        v.push("zone".into());
-        v.push(("net", &self.net).into());
-        v.push(("net_name", &self.net_name).into());
+        let mut v = Sexp::start("zone");
+        v.push(("net", &self.net));
+        v.push(("net_name", &self.net_name));
         for o in &self.other {
             v.push(o.clone());
         }
-        v.into()
+        v
     }
 }
 
 impl IntoSexp for Net {
     fn into_sexp(&self) -> Sexp {
-        let mut v = vec![];
-        v.push("net".into());
-        v.push(self.num.into());
-        v.push(self.name.clone().into());
-        v.into()
+        let mut v = Sexp::start("net");
+        v.push(self.num);
+        v.push(&self.name);
+        v
     }
 }
 
 impl IntoSexp for NetClass {
     fn into_sexp(&self) -> Sexp {
-        let mut v = vec![];
-        v.push("net_class".into());
-        v.push(self.name.clone().into());
-        v.push(self.desc.clone().into());
-        v.push(("clearance", &self.clearance).into());
-        v.push(("trace_width", &self.trace_width).into());
-        v.push(("via_dia", &self.via_dia).into());
-        v.push(("via_drill", &self.via_drill).into());
-        v.push(("uvia_dia", &self.uvia_dia).into());
-        v.push(("uvia_drill", &self.uvia_drill).into());
+        let mut v = Sexp::start("net_class");
+        v.push(&self.name);
+        v.push(&self.desc);
+        v.push(("clearance", &self.clearance));
+        v.push(("trace_width", &self.trace_width));
+        v.push(("via_dia", &self.via_dia));
+        v.push(("via_drill", &self.via_drill));
+        v.push(("uvia_dia", &self.uvia_dia));
+        v.push(("uvia_drill", &self.uvia_drill));
         if let Some(diff_pair_gap) = self.diff_pair_gap {
-            v.push(("diff_pair_gap", &diff_pair_gap).into());
+            v.push(("diff_pair_gap", &diff_pair_gap));
         }
         if let Some(diff_pair_width) = self.diff_pair_width {
-            v.push(("diff_pair_width", &diff_pair_width).into());
+            v.push(("diff_pair_width", &diff_pair_width));
         }
         for net in &self.nets {
-            v.push(("add_net", net).into());
+            v.push(("add_net", net));
         }
-        v.into()
+        v
     }
 }
 
@@ -175,80 +167,74 @@ impl IntoSexp for SetupElement {
 
 impl IntoSexp for Setup {
     fn into_sexp(&self) -> Sexp {
-        let mut v = vec![];
-        v.push("setup".into());
+        let mut v = Sexp::start("setup");
         for ref k in &self.elements {
             v.push(k.into_sexp())
         }
-        let mut v2 = vec![];
-        v2.push("pcbplotparams".into());
+        let mut v2 = Sexp::start("pcbplotparams");
         for ref k in &self.pcbplotparams {
             v2.push(k.into_sexp())
         }
-        v.push(v2.into());
-        v.into()
+        v.push(v2);
+        v
     }
 }
 
 impl IntoSexp for GrText {
     fn into_sexp(&self) -> Sexp {
-        let mut v = vec![];
-        v.push("gr_text".into());
-        v.push(self.value.clone().into());
+        let mut v = Sexp::start("gr_text");
+        v.push(&self.value);
         v.push(self.at.into_sexp());
-        v.push(("layer", &self.layer).into());
+        v.push(("layer", &self.layer));
         v.push(self.effects.into_sexp());
-        v.into()
+        v
     }
 }
 
 impl IntoSexp for GrLine {
     fn into_sexp(&self) -> Sexp {
-        let mut v = vec![];
-        v.push("gr_line".into());
+        let mut v = Sexp::start("gr_line");
         v.push(self.start.into_sexp());
         v.push(self.end.into_sexp());
-        v.push(("angle", &self.angle).into());
-        v.push(("layer", &self.layer).into());
-        v.push(("width", &self.width).into());
-        v.into()
+        v.push(("angle", &self.angle));
+        v.push(("layer", &self.layer));
+        v.push(("width", &self.width));
+        v
     }
 }
 
 impl IntoSexp for GrArc {
     fn into_sexp(&self) -> Sexp {
-        let mut v = vec![];
-        v.push("gr_arc".into());
+        let mut v = Sexp::start("gr_arc");
         v.push(self.start.into_sexp());
         v.push(self.end.into_sexp());
-        v.push(("angle", &self.angle).into());
-        v.push(("layer", &self.layer).into());
-        v.push(("width", &self.width).into());
-        v.into()
+        v.push(("angle", &self.angle));
+        v.push(("layer", &self.layer));
+        v.push(("width", &self.width));
+        v
     }
 }
 
 impl IntoSexp for Dimension {
     fn into_sexp(&self) -> Sexp {
-        let mut v = vec![];
-        v.push("dimension".into());
-        v.push(self.name.clone().into());
-        v.push(("width", &self.width).into());
-        v.push(("layer", &self.layer).into());
+        let mut v = Sexp::start("dimension");
+        v.push(&self.name);
+        v.push(("width", &self.width));
+        v.push(("layer", &self.layer));
         match self.tstamp {
             None => (),
             Some(ref tstamp) => {
-                v.push(("tstamp", tstamp).into());
+                v.push(("tstamp", tstamp));
             }
         }
         v.push(self.text.into_sexp());
-        v.push(Sexp::new_named_sexp("feature1", &self.feature1));
-        v.push(Sexp::new_named_sexp("feature2", &self.feature2));
-        v.push(Sexp::new_named_sexp("crossbar", &self.crossbar));
-        v.push(Sexp::new_named_sexp("arrow1a", &self.arrow1a));
-        v.push(Sexp::new_named_sexp("arrow1b", &self.arrow1b));
-        v.push(Sexp::new_named_sexp("arrow2a", &self.arrow2a));
-        v.push(Sexp::new_named_sexp("arrow2b", &self.arrow2b));
-        v.into()
+        v.push(("feature1", self.feature1.into_sexp()));
+        v.push(("feature2", self.feature2.into_sexp()));
+        v.push(("crossbar", self.crossbar.into_sexp()));
+        v.push(("arrow1a", self.arrow1a.into_sexp()));
+        v.push(("arrow1b", self.arrow1b.into_sexp()));
+        v.push(("arrow2a", self.arrow2a.into_sexp()));
+        v.push(("arrow2b", self.arrow2b.into_sexp()));
+        v
     }
 }
