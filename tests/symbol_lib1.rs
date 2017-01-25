51722b36b53aa057d758077d0b7123c342368c75
Joost Yervante Damad <joost@damad.be>
2017-01-25 09:53:28 +0100

    add symbol_lib parse_and_compare test

--- tests/symbol_lib1.rs
+++ tests/symbol_lib1.rs
@@ -1,16 +1,25 @@
 // (c) 2016-2017 Joost Yervante Damad <joost@productize.be>
 
-use std::path::PathBuf;
+extern crate kicad_parse_gen as kicad;
 
-extern crate kicad_parse_gen;
+extern crate difference;
 
-fn main() {
+#[test]
+fn parse_and_compare() {
     let mut file_name = String::new();
     file_name.push_str(env!("CARGO_MANIFEST_DIR"));
     file_name.push_str("/tests/data/");
     file_name.push_str("breakout-cache.lib");
     
-    let path = PathBuf::from(&file_name);
-    let s = kicad_parse_gen::symbol_lib::parse_file(&path).unwrap();
-    println!("{}", s);
+    let content = kicad::read_file(&file_name).unwrap();
+    
+    let s = kicad::read_symbol_lib(&file_name).unwrap();
+    let s = format!("{}", s);
+
+    // very inefficient...
+    let (n, _) = difference::diff(&content, &s, "\n");
+    if n > 1 {
+        difference::print_diff(&content, &s, "\n");
+        assert_eq!(n, 1);
+    }
 }
