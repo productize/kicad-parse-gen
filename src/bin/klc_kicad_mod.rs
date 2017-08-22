421c162ce3eea6f6aade0c1394e1723434670f24
Joost Yervante Damad <joost@damad.be>
2017-08-22 13:48:48 +0200

    add test apps

---
+++ src/bin/klc_kicad_mod.rs
@@ -0,0 +1,29 @@
+// (c) 2017 Productize SPRL <joost@productize.be>
+
+extern crate kicad_parse_gen as kicad;
+#[macro_use]
+extern crate log;
+extern crate env_logger;
+
+use std::path::PathBuf;
+use std::env;
+
+use kicad::klc::KLCCheck;
+
+fn main() {
+    env::set_var("RUST_LOG", "debug");
+    env_logger::init().unwrap();
+
+    let mut args = std::env::args();
+    args.next();
+    let name = args.next().unwrap();
+    let name = PathBuf::from(name);
+    let module = kicad::read_module(&name).unwrap();
+    let checkres = module.check();
+    if !checkres.is_empty() {
+        info!("Module {}", module.name);
+        for v in checkres {
+            v.dump_on_logger(1);
+        }
+    }
+}
---
+++ src/bin/klc_symbol_lib.rs
@@ -0,0 +1,31 @@
+// (c) 2017 Productize SPRL <joost@productize.be>
+
+extern crate kicad_parse_gen as kicad;
+#[macro_use]
+extern crate log;
+extern crate env_logger;
+
+use std::path::PathBuf;
+use std::env;
+
+use kicad::klc::KLCCheck;
+
+fn main() {
+    env::set_var("RUST_LOG", "debug");
+    env_logger::init().unwrap();
+
+    let mut args = std::env::args();
+    args.next();
+    let name = args.next().unwrap();
+    let name = PathBuf::from(name);
+    let symbol_lib = kicad::read_symbol_lib(&name).unwrap();
+    for symbol in symbol_lib.symbols {
+        let checkres = symbol.check();
+        if !checkres.is_empty() {
+            info!("Symbol {}", symbol.name);
+            for v in checkres {
+                v.dump_on_logger(1);
+            }
+        }
+    }
+}
